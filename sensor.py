925c705464f733868199f91a3d7d1c2c13e30000
Hajdu Zoltán <90104142+ZoltanHajdu@users.noreply.github.com>
2023-04-20 01:17:09 +0200

    More sensor data

--- sensor.py
+++ sensor.py
@@ -3,13 +3,21 @@ import datetime
 
 
 class Sensor:
+
+    lower_bound = 0
+    upper_bound = 100
+    steps = 1
+    round = 5
+
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
-        sensor_data = random.randrange(0, 100, 1)
         sensor_date = datetime.datetime.now().strftime("%H:%M:%S")
-        return "{name}: {date} {data}".format(name=self.name, date=sensor_date, data=sensor_data)
+        sensor_data1 = random.randrange(self.lower_bound, self.upper_bound, self.steps)
+        sensor_data2 = round(random.SystemRandom().uniform(self.lower_bound, self.upper_bound), self.round)
+        return "{name}: {date} {data1} {data2}".\
+            format(name=self.name, date=sensor_date, data1=sensor_data1, data2=sensor_data2)
 
 
 sensor = Sensor('tv')
