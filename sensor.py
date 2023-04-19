08ae88d9df734da671f323a5df70776393d49730
Hajdu Zoltán <90104142+ZoltanHajdu@users.noreply.github.com>
2023-04-19 21:01:07 +0200

    New value each call

--- sensor.py
+++ sensor.py
@@ -3,16 +3,15 @@ import datetime
 
 
 class Sensor:
-
-    sensor_data = random.randrange(0, 100, 1)
-    sensor_date = datetime.datetime.now().strftime("%H:%M:%S")
-
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
-        return "{name}: {date} {data}".format(name=self.name, date=Sensor.sensor_date, data=Sensor.sensor_data)
+        sensor_data = random.randrange(0, 100, 1)
+        sensor_date = datetime.datetime.now().strftime("%H:%M:%S")
+        return "{name}: {date} {data}".format(name=self.name, date=sensor_date, data=sensor_data)
 
 
 sensor = Sensor('tv')
+print(sensor)
 print(sensor)
